f7053093b40ed96ef28281de3b4565008edfd567
Rahul <122083504+RahulJaryalJ@users.noreply.github.com>
2025-03-09 05:34:09 -0700

    first commit

---
+++ db/connect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS:40000
+  })
+}
+
+module.exports = connectDB
